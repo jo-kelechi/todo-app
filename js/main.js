f7b0a09a5749a238e2c0704a7a56d4f8890565f3
Joseph Kelechi <77107025+jo-kelechi@users.noreply.github.com>
2021-03-08 09:42:50 +0100

    Modified Todo App
    Added Todo Filters

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ function newTodo(event) {
       countTodos();
    };
    li.appendChild(span);
+   addClass(li, "show");
    countTodos();
 }
 
@@ -48,3 +49,37 @@ function countTodos() {
    }
    document.getElementById('todoCounter').innerText = e.length;
 }
+
+// Added filter methods
+filterTodos('all');
+function filterTodos(t) {
+   var x, i;
+   x = document.getElementsByclassName("todo-item");
+   if (t == "all") t = "";
+   for (i = 0; i < x.length; i++) {
+      removeClass(x[i], "show");
+      if (x[i].className.indexOf(t) > -1) addClass(x[i], "show");
+   }
+}
+
+function addClass(element, name) {
+   var i, arr1, arr2;
+   arr1 = element.className.split(" ");
+   arr2 = name.split(" ");
+   for (i = 0; i < arr2.length; i++) {
+     if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+   }
+}
+
+function removeClass(element, name) {
+   var i, arr1, arr2;
+   arr1 = element.className.split(" ");
+   arr2 = name.split(" ");
+   for (i = 0; i < arr2.length; i++) {
+      while (arr1.indexOf(arr2[i]) > -1) {
+         arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      }
+   }
+   element.className = arr1.join(" ");
+}
+
